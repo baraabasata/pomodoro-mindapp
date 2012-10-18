9fa96f28724da1aa72cc5b42dc4c3205ba73c452
Baraa Basata <baraa.basata@contractor.cengage.com>
2012-10-18 11:08:25 -0400

    rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PomodoroMindapp::Application.config.secret_token = '99da44cfdc7c4e78f1881b9a066581c6d82789aeadcbcdc31621b3880e24ae9c3611c4aa357b982d121dca3ae934f609772a2e8e46993c95f2dc693a8eea6f61'
